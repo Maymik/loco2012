e192ccdf57462d4c8b2f87b22de783d1338777de
Mike <mykhaylo.kozyrev@kallisto.dev>
2024-12-24 07:13:23 +0200

    integrated

--- lib/data/coaches_model.dart
+++ lib/data/coaches_model.dart
@@ -2,44 +2,47 @@ class Coach {
   final String position;
   final String name;
   final String info;
+  final String id;
 
   Coach({
     required this.position,
     required this.name,
     required this.info,
+    required this.id,
   });
 
   factory Coach.fromJson(Map<String, dynamic> json) {
     return Coach(
-      position: json['position'] as String,
-      name: json['name'] as String,
-      info: json['info'] as String,
-    );
+        position: json['position'] as String,
+        name: json['name'] as String,
+        info: json['info'] as String,
+        id: json['id'] as String);
   }
+
   Map<String, dynamic> toJson() {
     return {
       'position': position,
       'name': name,
       'info': info,
+      'id': id,
     };
   }
 }
 
 final List<Coach> coaches = [
   Coach(
-    position: 'Головний тренер',
-    name: 'Іван Іванов',
-    info: 'Має багаторічний досвід роботи з провідними командами.',
-  ),
+      position: 'Головний тренер',
+      name: 'Іван Іванов',
+      info: 'Має багаторічний досвід роботи з провідними командами.',
+      id: 'Головний тренер Іван Іванов id'),
   Coach(
-    position: 'Асистент',
-    name: 'Петро Петров',
-    info: 'Відповідає за розробку тактики та стратегії.',
-  ),
+      position: 'Асистент',
+      name: 'Петро Петров',
+      info: 'Відповідає за розробку тактики та стратегії.',
+      id: 'Асистент Петро Петров id'),
   Coach(
-    position: 'Тренер воротарів',
-    name: 'Сергій Сергієнко',
-    info: 'Спеціаліст з тренувань воротарів із багаторічним стажем.',
-  ),
+      position: 'Тренер воротарів',
+      name: 'Сергій Сергієнко',
+      info: 'Спеціаліст з тренувань воротарів із багаторічним стажем.',
+      id: 'Тренер воротарів Сергій Сергієнко id'),
 ];
-
--- lib/data/schedule_model.dart
+++ lib/data/schedule_model.dart
@@ -2,11 +2,13 @@ class Schedule {
   final String day;
   final String time;
   final String location;
+  final String id;
 
   Schedule({
     required this.day,
     required this.time,
     required this.location,
+    required this.id,
   });
 }
 
@@ -15,15 +17,18 @@ final List<Schedule> scheduleList = [
     day: 'Понеділок',
     time: '16:30',
     location: 'Стадіон Локомотив',
+    id: '1',
   ),
   Schedule(
     day: 'Середа',
     time: '16:30',
     location: 'Стадіон Локомотив',
+    id: '2',
   ),
   Schedule(
     day: 'П’ятниця',
     time: '16:30',
     location: 'Стадіон Локомотив',
+    id: '3',
   ),
 ];
