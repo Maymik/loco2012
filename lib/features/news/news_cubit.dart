e7ec1d92119204661b5d3ebd11fee0c40110cb15
Mike <mykhaylo.kozyrev@kallisto.dev>
2025-01-11 23:20:17 +0200

    temp_commit

--- lib/features/news/news_cubit.dart
+++ lib/features/news/news_cubit.dart
@@ -1,34 +1,3 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'news_state.dart';
-// import '../../data/news_model.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-//
-// class NewsCubit extends Cubit<NewsState> {
-//   NewsCubit() : super(const NewsState.initial()) {
-//     subscribeToNews();
-//   }
-//
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//
-//   void subscribeToNews() {
-//     emit(const NewsState.loading());
-//
-//     _firestore
-//         .collection('news')
-//         .orderBy('time', descending: true)
-//         .snapshots()
-//         .listen((querySnapshot) {
-//       final newsList = querySnapshot.docs.map((doc) {
-//         final data = doc.data();
-//         return NewsModel.fromJson(data, doc.id);
-//       }).toList();
-//
-//       emit(NewsState.loaded(newsList));
-//     }, onError: (e) {
-//       emit(NewsState.error('Помилка завантаження: ${e.toString()}'));
-//     });
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../data/news_model.dart';
