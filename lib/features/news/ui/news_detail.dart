76e4d15438a478c9e8c0b2a4a68bf048e3cc8fa2
Mike <mykhaylo.kozyrev@kallisto.dev>
2025-01-18 23:15:27 +0200

    temp_commit

--- lib/features/news/ui/news_detail.dart
+++ lib/features/news/ui/news_detail.dart
@@ -9,6 +9,7 @@ class NewsDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.green,
       appBar: AppBar(
         title: Text(news.news.split('.').first),
         leading: IconButton(
