ab6dcef4c4fa150cbe6d1312e0cf108c690ee0b3
Mike <mykhaylo.kozyrev@kallisto.dev>
2025-01-23 09:14:49 +0200

    added_custom_app_bar

--- lib/features/news/ui/news_screen.dart
+++ lib/features/news/ui/news_screen.dart
@@ -1,14 +1,12 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 import '../../../utils/constants.dart';
+import '../../../widgets/custom_app_bar.dart';
 import '../../../widgets/custom_circular_indicator.dart';
 import '../cubit/news_cubit.dart';
-import 'news_detail.dart';
 import '../cubit/news_state.dart';
-
-@RoutePage(name: 'NewsRoute')
+import 'news_detail.dart';
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
@@ -17,46 +15,14 @@ class NewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      appBar:
-      AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        backgroundColor: Colors.red,
-        title: const Text(
-          AppConstants.news,
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 40,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              if (value == 'Instagram') {
-                launchUrl(Uri.parse('https://www.instagram.com/lokomotyv.ua/'));
-              } else if (value == 'Facebook') {
-                launchUrl(Uri.parse('https://www.facebook.com/lokomotyv2012/'));
-              }
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: 'Instagram',
-                child: Text('Ми в Instagram'),
-              ),
-              const PopupMenuItem(
-                value: 'Facebook',
-                child: Text('Ми в Facebook'),
-              ),
-            ],
-          ),
-        ],
+      appBar: const CustomAppBar(
+        title: AppConstants.news,
       ),
       body: BlocBuilder<NewsCubit, NewsState>(
         builder: (context, state) {
           return state.when(
             initial: () => const Center(child: Text('')),
-            loading: () =>  const Center(child: FootballLoadingIndicator()),
+            loading: () => const Center(child: FootballLoadingIndicator()),
             loaded: (newsList) => ListView.builder(
               itemCount: newsList.length,
               itemBuilder: (context, index) {
--- lib/features/schedule/ui/schedule_screen.dart
+++ lib/features/schedule/ui/schedule_screen.dart
@@ -1,15 +1,13 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../../utils/constants.dart';
+import '../../../widgets/custom_app_bar.dart';
 import '../../../widgets/custom_circular_indicator.dart';
 import '../../../widgets/custom_expandable_card.dart';
 import '../cubit/schedule_cubit.dart';
 import '../cubit/schedule_state.dart';
 
-@RoutePage(name: 'ScheduleRoute')
 class ScheduleScreen extends StatelessWidget {
   const ScheduleScreen({super.key});
 
@@ -17,39 +15,8 @@ class ScheduleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        backgroundColor: Colors.red,
-        title: const Text(
-          AppConstants.schedule,
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 40,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              if (value == 'Instagram') {
-                launchUrl(Uri.parse('https://www.instagram.com/lokomotyv.ua/'));
-              } else if (value == 'Facebook') {
-                launchUrl(Uri.parse('https://www.facebook.com/lokomotyv2012/'));
-              }
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: 'Instagram',
-                child: Text('Ми в Instagram'),
-              ),
-              const PopupMenuItem(
-                value: 'Facebook',
-                child: Text('Ми в Facebook'),
-              ),
-            ],
-          ),
-        ],
+      appBar: const CustomAppBar(
+        title: AppConstants.schedule,
       ),
       body: BlocBuilder<ScheduleCubit, ScheduleState>(
         builder: (context, state) {
